a15b799c4c717bd6c90b747a36f70638c55ec7df
p2327 <desaru@protonmail.com>
2020-02-13 09:09:09 +0000

    updated day02

--- day02.py
+++ day02.py
@@ -1,12 +1,71 @@
 from typing import List, Iterator
 from pathlib import Path
+from collections import Counter
+from itertools import repeat
 
 
-data_folder = Path('/Users/pietro.pravettoni/developer/adventofcode/data/')
+data_folder = Path('/pythonprojects/adventofcode/data/')
 
 
 with open(data_folder / 'box_ids.txt') as f:
-    ids = [line.split() for line in f]
+    ids = [line.strip() for line in f]
 
 
-def repeats 
+def two_and_threes(codes: List[str]) -> int:
+    twos = 0
+    threes = 0
+    
+    for code in codes:
+        zeros = list(repeat(0, len(code)))
+        counts = dict(zip(code, zeros))
+        
+        for letter in code:
+            if letter in counts.keys():
+                counts[letter] += 1
+
+        occurences = set(counts.values())
+        
+        for number in occurences:
+            if number == 2:
+                twos += 1
+            elif number == 3:
+                threes += 1
+            else: 
+                pass            
+    
+    return twos * threes
+
+
+# part 1 answer
+print(two_and_threes(ids))
+
+
+def common_letters
+
+
+def all_codes(codes: List[int], start: int = 0) -> Iterator[int]:
+    """
+    Generate all codes by adding the number in a cycle
+    Start at 0
+    """
+    code = start
+
+    while True:
+        for code in codes:
+            yield code
+            # next execution resumes from this point
+            frequency = next()
+  
+
+def match(s1, s2):
+    assert len(s1) == len(s2), 'lenght must be equal'
+    ok = False
+
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            if ok:
+                return False
+            else:
+                ok = True
+
+    return ok
