14ea611b4032e0f95988bfed8fdc6874c1d25c28
Pietro Pravettoni <pietro.pravettoni@LON1-MACL-00362.local>
2020-02-14 16:14:01 +0000

    solved day 02

--- day01.py
+++ day01.py
@@ -1,4 +1,3 @@
-#import pandas as pd
 from typing import List, Iterator
 from pathlib import Path
 
@@ -25,9 +24,10 @@ def all_frequencies(numbers: List[int], start: int = 0) -> Iterator[int]:
             # next execution resumes from this point
             frequency += number
 
+
 def first_repeat_frequency(numbers: List[int], start: int = 0) -> int:
     seen = set()
-    
+
     for frequency in all_frequencies(numbers, start):
         if frequency in seen:
             return frequency
@@ -37,12 +37,10 @@ def first_repeat_frequency(numbers: List[int], start: int = 0) -> int:
 
 # test against the examples
 assert first_repeat_frequency([1, -1]) == 0
-assert first_repeat_frequency([+3, +3, +4, -2, -4 ]) == 10
-assert first_repeat_frequency([-6, +3, +8, +5, -6 ]) == 5
+assert first_repeat_frequency([+3, +3, +4, -2, -4]) == 10
+assert first_repeat_frequency([-6, +3, +8, +5, -6]) == 5
 assert first_repeat_frequency([+7, +7, -2, -7, -4]) == 14
 
 
 # solution 2
 print(first_repeat_frequency(frequencies))
-
-
--- day02.py
+++ day02.py
@@ -1,10 +1,21 @@
-from typing import List, Iterator
+from typing import List, Tuple
 from pathlib import Path
-from collections import Counter
+from collections import deque
 from itertools import repeat
 
+"""
+Part 1: find boxes with similar ids
 
-data_folder = Path('/pythonprojects/adventofcode/data/')
+Tasks:
+    - count the boxes that have exactly 2 repeated letters in their id
+    - do the same for 3 repeats
+
+Hint: multiple occurences of repeats count only once
+Hint: if an id contains 2s and 3s repeats it counts  towards both totals
+"""
+
+
+data_folder = Path('/Users/pietro.pravettoni/developer/adventofcode/data/')
 
 
 with open(data_folder / 'box_ids.txt') as f:
@@ -12,27 +23,33 @@ with open(data_folder / 'box_ids.txt') as f:
 
 
 def two_and_threes(codes: List[str]) -> int:
+    """
+    Counts the letter repetition in a box id string (codes).
+    Adds two and three repeats to a total count.
+    Multiplies the total to find the boxes checksum
+    """
     twos = 0
     threes = 0
-    
+
     for code in codes:
+        # set an hash table to keep count for each letter
         zeros = list(repeat(0, len(code)))
         counts = dict(zip(code, zeros))
-        
+
         for letter in code:
             if letter in counts.keys():
                 counts[letter] += 1
 
         occurences = set(counts.values())
-        
+
         for number in occurences:
             if number == 2:
                 twos += 1
             elif number == 3:
                 threes += 1
-            else: 
-                pass            
-    
+            else:
+                pass
+
     return twos * threes
 
 
@@ -40,32 +57,70 @@ def two_and_threes(codes: List[str]) -> int:
 print(two_and_threes(ids))
 
 
-def common_letters
+"""
+Part 2: identify the boxes that contain prototype fabric
+
+Tasks:
+    - Identify box ids which differs by just one letter
+    - Return the common letters
+
+Hint: only two boxes are the correct ones
+"""
+
+test_ids = [
+    'abcde',
+    'fghij',
+    'klmno',
+    'pqrst',
+    'fguij',
+    'axcye',
+    'wvxyz'
+]
 
 
-def all_codes(codes: List[int], start: int = 0) -> Iterator[int]:
+def identify(ids: List[str]) -> Tuple[Tuple[str], str]:
     """
-    Generate all codes by adding the number in a cycle
-    Start at 0
+    Takes a list of box ids and return the position of 
+    the two that differ by one letter
     """
-    code = start
+    ids_queue = deque(ids)
+    prototype_ids = ()
+    current_id = []
 
-    while True:
-        for code in codes:
-            yield code
-            # next execution resumes from this point
-            frequency = next()
-  
+    current_id = ids_queue.popleft()
 
-def match(s1, s2):
-    assert len(s1) == len(s2), 'lenght must be equal'
-    ok = False
+    def match(box_id):
+        nonlocal ids
+        nonlocal prototype_ids
+        nonlocal current_id
+        nonlocal ids_queue
+        # difference counter
+        diff_count = 0
 
-    for c1, c2 in zip(s1, s2):
-        if c1 != c2:
-            if ok:
-                return False
+        # compare the current id versus all the boxes ids
+        for id in ids:
+            for c1, c2 in zip(box_id, id):
+                if c1 != c2:
+                    diff_count += 1
+                else:
+                    diff_count += 0
+
+            if diff_count == 1:
+                prototype_ids += (box_id, id)
+                # the commented gives the right answer but changes
+                # sequence order so the answer is not valid
+                # equal_letters = ''.join(set(box_id).intersection(id))
+                equal_letters = box_id.replace(list(set(box_id).difference(id))[0], '')
+                return prototype_ids, equal_letters
             else:
-                ok = True
+                # onto then next
+                pass
+            # reset the counter for the next id
+            diff_count = 0
+        # process the next id if not succesful
+        return match(ids_queue.popleft())
 
-    return ok
+    return match(current_id)
+
+
+print(identify(ids))
